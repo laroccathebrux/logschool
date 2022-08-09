8ef0869a64063158de6b6b7b2e929b271f67aaa0
larocca_thebrux <larocca_thebrux@yahoo.com.br>
2022-08-09 18:05:29 -0300

    local save

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from distutils.log import error
 import requests
 import configparser
 import pandas as pd
-from azure.storage.blob import BlobServiceClient
+import json
 
 def apiAccess(KEY, URL, params):
     headers = {
@@ -21,21 +21,7 @@ def apiAccess(KEY, URL, params):
 
     return df_tmp
 
-def getAzureAccess(config):
-    ACCKEY  = config["AZURE"]["ACCKEY"]
-    CONTAINER = config["AZURE"]["CONTAINER"]
-    blob_service_client = BlobServiceClient.from_connection_string(ACCKEY)
-    container_client = blob_service_client.get_container_client(CONTAINER)
-    return container_client
-
-def saveCSVAzure(df, name, container_client):
-    output = df.to_csv(index=False, encoding="utf-8")
-    blob_client = container_client.get_blob_client(name)
-    blob_client.upload_blob(output, blob_type="BlockBlob", overwrite=True)
-
-def getStudent(KEY, df, config):
-    URL = "https://ead.logschool.com.br/api/1/student"
-    
+def getData(KEY, df, config, URL, name):
 
     for i in range(0, 1000, 1000):
         params = {
@@ -43,79 +29,8 @@ def getStudent(KEY, df, config):
         }
         df_tmp  = apiAccess(KEY, URL, params)
         df = pd.concat([df_tmp, df])
-
-    container_client = getAzureAccess(config)
-    saveCSVAzure(df, "logschool_student.csv", container_client)
-
-def getPermission(KEY, df, config):
-    URL = "https://ead.logschool.com.br/api/1/permission"
-    
-
-    for i in range(0, 1000, 1000):
-        params = {
-            "offset": f"{i}"
-        }
-        df_tmp  = apiAccess(KEY, URL, params)
-        df = pd.concat([df_tmp, df])
-
-    container_client = getAzureAccess(config)
-    saveCSVAzure(df, "logschool_permission.csv", container_client)
-
-def getUserPermission(KEY, df, config):
-    URL = "https://ead.logschool.com.br/api/1/userpermission"
-    
-
-    for i in range(0, 1000, 1000):
-        params = {
-            "offset": f"{i}"
-        }
-        df_tmp  = apiAccess(KEY, URL, params)
-        df = pd.concat([df_tmp, df])
-
-    container_client = getAzureAccess(config)
-    saveCSVAzure(df, "logschool_userpermission.csv", container_client)
-
-def getCategory(KEY, df, config):
-    URL = "https://ead.logschool.com.br/api/1/category"
-    
-
-    for i in range(0, 1000, 1000):
-        params = {
-            "offset": f"{i}"
-        }
-        df_tmp  = apiAccess(KEY, URL, params)
-        df = pd.concat([df_tmp, df])
-
-    container_client = getAzureAccess(config)
-    saveCSVAzure(df, "logschool_category.csv", container_client)
-
-def getCourse(KEY, df, config):
-    URL = "https://ead.logschool.com.br/api/1/course"
-    
-
-    for i in range(0, 1000, 1000):
-        params = {
-            "offset": f"{i}"
-        }
-        df_tmp  = apiAccess(KEY, URL, params)
-        df = pd.concat([df_tmp, df])
-
-    container_client = getAzureAccess(config)
-    saveCSVAzure(df, "logschool_course.csv", container_client)
-
-def getModule(KEY, df, config):
-    URL = "https://ead.logschool.com.br/api/1/module"
-    
-
-    for i in range(0, 1000, 1000):
-        params = {
-            "offset": f"{i}"
-        }
-        df_tmp  = apiAccess(KEY, URL, params)
-        df = pd.concat([df_tmp, df])
-
-    container_client = getAzureAccess(config)
-    saveCSVAzure(df, "logschool_module.csv", container_client)
+    print(f"{name} Saved!")
+    df.to_csv(f"csv/{name}")
 
 def main():
 
@@ -124,15 +39,15 @@ def main():
 
     KEY = config["API"]["KEY"]
     df = pd.DataFrame()
-    
-    getStudent(KEY, df, config)
-    getPermission(KEY, df, config)
-    getUserPermission(KEY, df, config)
-    getCategory(KEY, df, config)
-    getCourse(KEY, df, config)
-    getModule(KEY, df, config)
-    
-        
+
+    with open("dictAPI.txt") as f:
+        data = f.read()
+      
+    dictAPI = json.loads(data)
+    print("Getting Data...")
+    for x in dictAPI.values():
+        getData(KEY, df, config, x['URL'], x['NAME'])
+    print("Finished")
 
 if __name__ == "__main__":
     main()
