bf2ba2234948c8bd468c4fc1067f91a7444ead44
larocca_thebrux <larocca_thebrux@yahoo.com.br>
2022-08-09 11:04:59 -0300

    First Commit

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import requests
+import configparser
+
+config = configparser.ConfigParser()
+config.read("credentials.ini")
+
+KEY = config["API"]["KEY"]
+URL = "https://ead.logschool.com.br/api/1/student"
+
+headers = {"Accept": "application/json", "Token": KEY}
+
+r = requests.get(url=URL, headers=headers)
+
+print(r.content)
